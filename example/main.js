efc6acde7af3d7f70bb23a7a6272cdeb00839f07
Marek "foxie" Brenčič <marek.brencic@lurity.com>
2020-11-06 14:03:25 +0100

    updated dependencies + maps available in dev mode

--- example/main.js
+++ example/main.js
@@ -1,20 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
 // ------------------------------
 // x5-modal plugin install START
 // ------------------------------
-import x5GMaps from '../src'
+
+const GOOGLE_KEY = process.env.VUE_APP_GOOGLE_KEY
+    ? process.env.VUE_APP_GOOGLE_KEY
+    : "";
+
+import x5GMaps from "../src";
 Vue.use(x5GMaps, {
-  key: process.env.VUE_APP_GOOGLE_KEY,
-  libraries: ['visualization'],
-})
+    key: GOOGLE_KEY,
+    libraries: ["visualization"],
+});
+
 // ------------------------------
 // x5-modal plugin install END
 // ------------------------------
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
